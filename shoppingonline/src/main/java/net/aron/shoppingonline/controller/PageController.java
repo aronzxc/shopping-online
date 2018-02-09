f82f4e2a2c6ffb3a33e8cc571f7b29a95623fee5
Aaron Claudio <aronzxc@gmail.com>
2018-02-09 10:10:40 +0800

    page controller

--- shoppingonline/src/main/java/net/aron/shoppingonline/controller/PageController.java
+++ shoppingonline/src/main/java/net/aron/shoppingonline/controller/PageController.java
@@ -1,8 +1,8 @@
 package net.aron.shoppingonline.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,8 +17,18 @@ public class PageController {
 		return mv;
 	}
 	
-	@RequestMapping(value="/test")
-	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+//		if(greeting == null) {
+//			greeting = "Hello from the other side";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting) {
 		if(greeting == null) {
 			greeting = "Hello from the other side";
 		}
@@ -26,6 +36,5 @@ public class PageController {
 		mv.addObject("greeting",greeting);
 		return mv;
 	}
-	
 
 }
